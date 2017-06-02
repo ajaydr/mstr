f712127b6bdef349543cb08227bb68ded61718d2
Ajay <ajay.rajgure@gmail.com>
2017-06-02 16:11:31 -0400

    latest.

--- BriefingBook.java
+++ BriefingBook.java
@@ -7,15 +7,13 @@ import com.microstrategy.web.objects.WebPrompts;
 import com.microstrategy.web.objects.rw.*;
 
 import java.io.File;
-
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.sql.Connection;
-
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
 import java.sql.Statement;
 import java.util.ResourceBundle;
 
@@ -26,13 +24,15 @@ public class BriefingBook {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		
+	
 		String dbURL;
-		String pdfFileLoc;
+		String mstrWebAppFolderLoc;
+		String pdfFileFolder;
 		String projectName;
 		String userName;
 		String pwd;
-		String serverName;
+		String iServerName;
+		String webRootURL;
 		
 		String pdfFileName ="Doc";
 		RWInstance rwi=null;
@@ -47,7 +47,7 @@ public class BriefingBook {
 		String getBookDocsSQL="select DocId, DocParamStr from dbo.BriefingBookReqDocs where BookID=";
 		String updateBookSQL="update dbo.BriefingBookRequest set BookStatus='In-Progress' where BookID=";
 		String updateBookRevertSQL="update dbo.BriefingBookRequest set BookStatus='Submitted' where BookID=";
-		String updateBookCompletionSQL="update dbo.BriefingBookRequest set BookStatus='Complete', BookFileURL='#LOC#' where BookID=";
+		String updateBookCompletionSQL="update dbo.BriefingBookRequest set BookStatus='Complete', BookFileURL='<a href=''#WEBROOTURL#/#PDFFILEFOLDER#/#FILE_NAME#'' target=''_blank''>Click</a>' where BookID=";
 
 		int numPdfs=0;
 		String BookID=null;
@@ -58,11 +58,13 @@ public class BriefingBook {
 		try {
 			ResourceBundle propertiesFile = ResourceBundle.getBundle("BriefingBook");
 			dbURL=propertiesFile.getString("dbURL");
-			pdfFileLoc=propertiesFile.getString("pdfFileLoc");
+			mstrWebAppFolderLoc=propertiesFile.getString("mstrWebAppFolderLoc")+"/";
+			pdfFileFolder=propertiesFile.getString("pdfFileFolder")+"/";
 			projectName=propertiesFile.getString("projectName");
 			userName=propertiesFile.getString("userName");
 			pwd=propertiesFile.getString("pwd");
-			serverName=propertiesFile.getString("serverName");
+			iServerName=propertiesFile.getString("iServerName");
+			webRootURL=propertiesFile.getString("webRootURL");
 
 			conn = DriverManager.getConnection(dbURL);
 			stmt = conn.createStatement();
@@ -71,31 +73,31 @@ public class BriefingBook {
             while ( rs.next() ) {
             	BookID = rs.getString("BookID");
             	BookName = rs.getString("BookName");
-                System.out.println("Processing Book:"+BookID);
+                System.out.println("Processing Book Name: ID "+BookName+" : "+BookID);
             }
             if(BookID != null)
+            {
             	stmt.executeUpdate(updateBookSQL+BookID);
             
-            objectFactory = WebObjectsFactory.getInstance();
-			serverSession = objectFactory.getIServerSession();
-			//serverSession.setApplicationType(EnumDSSXMLApplicationType.DssXmlApplicationCustomApp);
-			serverSession.setAuthMode(EnumDSSXMLAuthModes.DssXmlAuthStandard);
-			serverSession.setProjectName(projectName);
-			serverSession.setLogin(userName);
-			serverSession.setPassword(pwd);
-			serverSession.setServerName(serverName);
-			if(BookID != null)
-            {
+            	objectFactory = WebObjectsFactory.getInstance();
+            	serverSession = objectFactory.getIServerSession();
+            	//serverSession.setApplicationType(EnumDSSXMLApplicationType.DssXmlApplicationCustomApp);
+            	serverSession.setAuthMode(EnumDSSXMLAuthModes.DssXmlAuthStandard);
+            	serverSession.setProjectName(projectName);
+            	serverSession.setLogin(userName);
+            	serverSession.setPassword(pwd);
+            	serverSession.setServerName(iServerName);
+
             	rs=stmt.executeQuery(getBookDocsSQL+BookID);
             	i=0;
             	while ( rs.next() ) {
             		i++;
             		String DocID = rs.getString("DocID");
             		String promptAnsXml=rs.getString("DocParamStr");
-            		System.out.println(DocID);
-            		System.out.println(promptAnsXml);
+            		System.out.println("Generating Document:"+DocID+"\n with promptxml:"+promptAnsXml);
+  
             		if(DocID != null){
-	            		rwi = objectFactory.getRWSource().getNewInstance(DocID);//("D389C4B94E5394E7A66C9A941AE14A6F");
+	            		rwi = objectFactory.getRWSource().getNewInstance(DocID);
 	        			//rwi.setAsync(false);
 	        			rwi.setMaxWait(-1);
 	        			rwi.setPollingFrequency(250); 
@@ -107,11 +109,11 @@ public class BriefingBook {
 	        			prompts.populateAnswers(promptAnsXml);
 	        			prompts.validate();
 	        			prompts.answerPrompts();
-	        			System.out.println("Prompts answered");
+	        			//System.out.println("Prompts answered");
 	        			//RWExportSettings expSet = rwi.getExportSettings();
 	        			//expSet.setGridKey("NODEKEY_OF_THE_GRID");
-	        			file = new File(pdfFileLoc+pdfFileName+i+".pdf");
-	        			// if file doesnt exists, then create it
+	        			file = new File(mstrWebAppFolderLoc+pdfFileFolder+pdfFileName+i+".pdf");
+	        			// if file doesn't exists, then create it
 	        			if (!file.exists()) {
 	        				file.createNewFile();
 	        			}
@@ -126,78 +128,27 @@ public class BriefingBook {
             		}
             		
             	}
-            }
-			
-			/*rwi = objectFactory.getRWSource().getNewInstance("D389C4B94E5394E7A66C9A941AE14A6F");//("D389C4B94E5394E7A66C9A941AE14A6F");
-			//rwi.setAsync(false);
-			rwi.setMaxWait(-1);
-			rwi.setPollingFrequency(250); 
-			status = rwi.pollStatus();
-			rwi.setAsync(false);
-			rwi.setExecutionMode(EnumRWExecutionModes.RW_MODE_PDF);
-			//Ans prompts
-			prompts = rwi.getPrompts();
-			prompts.populateAnswers(promptAnsXml2);
-			prompts.validate();
-			prompts.answerPrompts();
-			System.out.println("Answered prompts for second document.");
-			//RWExportSettings expSet = rwi.getExportSettings();
-			//expSet.setGridKey("NODEKEY_OF_THE_GRID");
-			file = new File("C:/Users/Public/Doc2.pdf");
-			// if file doesnt exists, then create it
-			if (!file.exists()) {
-				file.createNewFile();
-			}
-			fos = new FileOutputStream(file);
-			rwi.setMaxWait(-1);
-			rwi.setPollingFrequency(250); 
-			status = rwi.pollStatus();
-			fos.write(rwi.getExportData());
-			fos.flush();
-			fos.close();
-			*/
+           
 			serverSession.closeSession();
 			PDFMergerUtility PDFmerger = new PDFMergerUtility();
-			PDFmerger.setDestinationFileName(pdfFileLoc+BookName+".pdf");
+			PDFmerger.setDestinationFileName(mstrWebAppFolderLoc+pdfFileFolder+BookName+".pdf");
 			PDDocument pfddoc;
 			i=0;
+			System.out.println("Number of individual Pdfs to be merged:"+numPdfs);
 			for(int j=0 ; j<numPdfs; j++)
 			{
 				i++;
-				file = new File(pdfFileLoc+pdfFileName+i+".pdf");
+				file = new File(mstrWebAppFolderLoc+pdfFileFolder+pdfFileName+i+".pdf");
 				pfddoc = PDDocument.load(file);
 				PDFmerger.addSource(file);
 				pfddoc.close();
 			}
 			PDFmerger.mergeDocuments(null);
-			//Loading an existing PDF document
-		    //file1 = new File("C:/Users/Public/Doc1.pdf");
-		    //PDDocument doc1 = PDDocument.load(file1);
-		       
-		    //file2 = new File("C:/Users/Public/Doc2.pdf");
-		    //PDDocument doc2 = PDDocument.load(file2);
-		         
-		    //Instantiating PDFMergerUtility class
-		    //PDFMergerUtility PDFmerger = new PDFMergerUtility();
-
-		    //Setting the destination file
-		    //PDFmerger.setDestinationFileName("C:/Users/Public/MergedDoc.pdf");
-
-		    //adding the source files
-		    //PDFmerger.addSource(file1);
-		    //PDFmerger.addSource(file2);
-
-		    //Merging the two documents
-		    //PDFmerger.mergeDocuments(null);
-		    //mergeDocuments();
-
-		     System.out.println("Documents merged");
-		     //Closing the documents
-		     //doc1.close();
-		     //doc2.close();
-		     updateBookCompletionSQL =updateBookCompletionSQL.replace("#LOC#", pdfFileLoc+BookName+".pdf")+BookID;
-		     System.out.println(updateBookCompletionSQL);
-		     stmt.executeUpdate(updateBookCompletionSQL);
+			System.out.println("Documents merged complete");
+		
+			updateBookCompletionSQL =updateBookCompletionSQL.replace("#WEBROOTURL#", webRootURL).replace("#PDFFILEFOLDER#", pdfFileFolder).replace("#FILE_NAME#", URLEncoder.encode(BookName,"UTF-8")+".pdf")+BookID;
+		    stmt.executeUpdate(updateBookCompletionSQL);
+          }
 		      
 		} catch (IOException  | WebObjectsException | SQLException e) {
 			if(stmt != null && BookID != null)
